bc61eb511e37c6bcc2b22765c3761bb8f0b312a7
Kalki767 <kalkidankidane3@gmail.com>
2025-07-25 10:09:43 +0300

    Update image fix

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,5 @@
 const Product = require('../models/Product');
-const cloudinary = require('../config/cloudinary');
+const { cloudinary } = require('../config/cloudinary');
 
 // Get all products
 const getProducts = async (req, res) => {
